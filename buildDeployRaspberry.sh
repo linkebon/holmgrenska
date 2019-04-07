e37d18fc7d01800b6514d908a2c27cc2d58b3834
xstahol <staffan.holmgren@tre.se>
2019-04-07 19:58:09 +0200

    fixed deploy scripts to raspberry pi

--- buildDeployRaspberry.sh
+++ buildDeployRaspberry.sh
@@ -8,12 +8,12 @@ TARGET_SERVER=raspberryhost
 ./build.sh
 echo '########### Deploying to prod ###########'
 echo 'Stopping and removing old app'
-ssh pi@${TARGET_SERVER} "${APP_SCRIPTS_HOME}/STOP_HOLMGRENSKA.sh;rm -rf ${APP_HOME}/${APP_NAME};"
+ssh pi@${TARGET_SERVER} "sudo ${APP_SCRIPTS_HOME}/STOP_HOLMGRENSKA.sh;rm -rf ${APP_HOME}/${APP_NAME};"
 
 echo 'Uploading new version'
 scp -rp build/holmgrenska pi@${TARGET_SERVER}:${APP_HOME}
 echo 'Upload done'
-ssh pi@${TARGET_SERVER} "${APP_SCRIPTS_HOME}/START_HOLMGRENSKA.sh;"
+ssh pi@${TARGET_SERVER} "sudo ${APP_SCRIPTS_HOME}/START_HOLMGRENSKA.sh;"
 echo '########### Deploying complete ###########'
 
 
